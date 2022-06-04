484f0b471cde5065fdcd094f9e58a63af27a3224
cyanidium1 <99473719+cyanidium1@users.noreply.github.com>
2022-06-04 16:28:00 +0300

    ok

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const galleryFull = document.querySelector('.gallery');
-// makeem a lightbox
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.append(lightbox);
-// delaem galery i otkrivaem
-for (let el of galleryItems) {
-    const a = document.createElement('a');
-    a.className = 'gallery__link'
-    const image = document.createElement('img');
-    image.src = el.preview;
-    image.alt = el.description;
-    image.className = 'gallery__image';
-    a.append(image);
-    galleryFull.append(a);
-    a.addEventListener('click', event => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img')
-        img.src = el.original;
-        img.style.width = '60vw';
-        while (lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild)
-        }
-        lightbox.append(img)
-    })
-}
-// zakrivaem
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget){return}
-    lightbox.classList.remove('active')
-})
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+let kek;
+
+gallery.insertAdjacentHTML('afterbegin', galleryItems
+.map(
+  items => `<div class="gallery__item"><a class="gallery__link" href="${items.original}"> <img
+                class="gallery__image"
+                src="${items.preview}"
+                data-source="${items.original}"
+                alt="${items.description}"/></a></div>`
+)
+.join(''));
+
+gallery.addEventListener('click', event=>{
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+    kek = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+    kek.show();
+  });
